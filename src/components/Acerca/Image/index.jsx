b7b66aa711f88efd7932c1674fd515464f49a5c8
Alvaro Peña <110061249+aalvaropc@users.noreply.github.com>
2024-09-29 02:42:02 -0500

    fix: update CSS styles for improved layout and spacing

--- src/components/Acerca/Image/index.jsx
+++ src/components/Acerca/Image/index.jsx
@@ -2,7 +2,7 @@ import "./Image.css";
 
 const Image = () => {
   return (
-    <img src="./img/devfest_r.jpg" className="width__imgg md__imgg" alt="GDG Ica" />
+    <img src="./img/devfest2024.jpg" className="width__imgg md__imgg" alt="GDG Ica" />
   );
 };
 
